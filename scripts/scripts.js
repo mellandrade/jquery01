126803f488e7ea00c21e863db0d267117c453a60
Mell <30361727+mellandrade@users.noreply.github.com>
2019-05-07 19:45:18 -0700

    Add files via upload
    update

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+/*eslint-env browser*/
+
+$(document).ready(function () {
+    "use strict";
+
+});
+
+$(document).ready(function() {
+    "use strict";
+    var border = $("h2").css("border-color", "#000", "1px");
+});
